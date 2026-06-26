a81536fbda5d3c15ca00c3b9a4157cb46e3934fd
cbumanglag15 <c.bumanglag.15@gmail.com>
2026-06-26 15:57:31 -0700

    Initial home page setup

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import MainNavBar from "../components/MainNavBar"
+
+export default function HomePage() {
+
+  return (
+    <div className="home-page">
+      <h1>Welcome to Hostd!</h1>
+      <div className="main-nav-bar">
+        <MainNavBar />
+      </div>
+    </div>
+  );
+}
