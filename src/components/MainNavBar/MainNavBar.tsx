1c8ad9a5241744b5ba80d7f1356526e6a47d0f3f
Clarence Bumanglag <c.bumanglag.15@gmail.com>
2026-07-03 15:36:02 -0700

    Hotel Search Box Skeleton (#7)

--- src/components/MainNavBar/MainNavBar.tsx
+++ src/components/MainNavBar/MainNavBar.tsx
@@ -5,7 +5,7 @@ export default function MainNavBar() {
             <nav className="mainNavBarLinks">
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/login">Login</a></li>
+                    <li><a href="/">Events</a></li>
                     <li><a href="/">Offers</a></li>
                     <li><a href="/">Hostd Rewards Club</a></li>
                     <li><a href="/">Products</a></li>
