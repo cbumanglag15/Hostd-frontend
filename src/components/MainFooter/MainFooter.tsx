311e3732d96dcf02a9f12f034c9350364076a382
Clarence Bumanglag <c.bumanglag.15@gmail.com>
2026-07-05 14:23:05 -0700

    Fixes for Footer and Auth (#10)

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -1,7 +1,7 @@
 export default function MainFooter() {
   return (
     <footer className="main-footer">
-      <p>&copy; 2024 Hostd. All rights reserved.</p>
+      <p>&copy; 2026 Hostd. All rights reserved.</p>
     </footer>
   );
 }
